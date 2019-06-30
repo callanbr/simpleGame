a05cfbbe27e60ca1cc0c6a26b1c877c15c8244e1
Christopher Brimhall <chrisallan1992@icloud.com>
2019-06-29 18:52:54 -0700

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var myScore;
 
 function startGame() {
     myGamePiece = new component(10, 10, "red", 10, 120);
-    myGamePiece.gravity = 0.05ß;
+    myGamePiece.gravity = 0.05;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
